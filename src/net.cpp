14759dc0066dfa06dee1fcc699a380ac6e4b4561
Arieru <ariel_saher@hotmail.com>
2017-12-06 01:10:15 -0300

    Seeds

--- src/net.cpp
+++ src/net.cpp
@@ -1171,18 +1171,14 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"pool.kaori.cl", "pool.kaori.cl"},
-    {"explorer.kaori.cl", "explorer.kaori.cl"},
-    {"dns1.kaori.cl", "dns1.kaori.cl"},
-    {"dns2.kaori.cl", "dns2.kaori.cl"},
+    {"170.239.86.225", "170.239.86.225"},
+    {"170.239.86.89", "170.239.86.89"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"pool.kaori.cl", "pool.kaori.cl"},
-    {"explorer.kaori.cl", "explorer.kaori.cl"},
-    {"dns1.kaori.cl", "dns1.kaori.cl"},
-    {"dns2.kaori.cl", "dns2.kaori.cl"},
+    {"170.239.86.225", "170.239.86.225"},
+    {"170.239.86.89", "170.239.86.89"},
     {NULL, NULL}
 };
 
